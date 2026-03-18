559cd41155d2db4c9056fe7c1c67b0b85fae79ee
JeongMin-Seo <dev.galgay@gmail.com>
2026-03-18 15:19:55 +0900

    add ch06

---
+++ ch06/for.js
@@ -0,0 +1,21 @@
+// console.log("javascript")
+// console.log("javascript")
+// console.log("javascript")
+// console.log("javascript")
+// console.log("javascript")
+
+// for 반복문 사용법
+// for (초기문; 조건식; 증감문) {
+//     반복내용
+// }
+for (let i = 0; i < 5; i++) {
+    console.log("javascript " + i);
+}
+
+// 초기문, 조건식, 증감문 모두 생략 가능하지만
+// 무한 루프 주의하여 적절히 사용해야 한다.
+// let i = 0;
+// for (; i < 5; ) {
+//     console.log("javascript " + i);
+//     i += 1;
+// }
